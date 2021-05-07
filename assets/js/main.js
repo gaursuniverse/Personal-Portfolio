8a57fbc39038b0e9cca93053e72c027b04b71cea
VISHAL GAUR <57058981+gaursuniverse@users.noreply.github.com>
2021-05-07 19:27:47 +0530

    Show menu js finally completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 /*===== SHOW MENU =====*/
 const showMenu = (toggleId,navId)=> {
-    const toggle = document.getElementById(toggleId)
-    const nav = document.getElementById(navId)
+    const toggle = document.getElementById(toggleId), nav = document.getElementById(navId)
 
     if (toggle && nav) {
         toggle.addEventListener('click',()=> {
@@ -9,6 +8,7 @@ const showMenu = (toggleId,navId)=> {
         })
     }
 }
+showMenu('nav-toggle','nav-menu')
 
 /*===== REMOVE MENU MOBILE =====*/
 
